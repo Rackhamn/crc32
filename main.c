feb4dcc8dde9087cb55f80cd3c5eaacd7c7e7c41
Carl Blumenthal <37241233+Rackhamn@users.noreply.github.com>
2023-10-14 14:09:29 +0200

    Update main.c

--- main.c
+++ main.c
@@ -4,24 +4,6 @@
 
 #include "crc32.h"
 
-// used to dump the crc32_table 
-void print_crc32_table(char * name, unsigned int poly, unsigned int * buffer, int size) {	
-	printf("\\\\ CRC32_POLY = 0x%08X (%s)\n", poly, get_endian_string_short());
-	printf("unsigned int %s[%i] = {\n\t", name, size);
-	int k = 0;
-	for(int i = 0; i < size; i++) {
-		printf("0x%08X, ", buffer[i]);
-		
-		// spacial formatting for sub 80 chars
-		k += 16;
-		if(k >= 80) {
-			printf("\n\t");
-			k = 0;
-		}
-	}
-	printf("\n};\n");
-}
-
 // on my linux machine (x86_64, little endian) the crc32 of "test_data" is 2827671980
 int main(int argc, char ** argv) {
 	printf(" === START CRC32 ===\n");
@@ -32,7 +14,7 @@ int main(int argc, char ** argv) {
 	// this does not need to be called IF the compiler used the precompiled table 
 	crc32_init(CRC32_POLY);
 	 
-	// print_crc32_table("crc32_256_table", 0xEDB88320, crc32_table, 256);
+	// print_crc32_table("crc32_256_table", 0xEDB88320);
 	
 	char * string;
 	unsigned int hash;
